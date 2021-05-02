8976cfb4836958b3b5b1f2466619dd88dd9d7acc
David Lane <david.mr.lane@gmail.com>
2021-05-02 08:41:17 +0100

    node tutorial edits

--- 01-node-tutorial/12-http.js
+++ 01-node-tutorial/12-http.js
@@ -23,10 +23,10 @@ const server = http.createServer((req, res) => {
   } else if (req.url === '/about') {
     res.end('Here is our short history')
   } else {
-    res.end(`
+    res.end(`<body>
     <h1>Oops!</h1>
     <p>We can't seem to find the page you are looking for</p>
-    <a href="/">back home</a>
+    <a href="/">back home</a></body>
     `)
   }
 })
